d603732838a2323344b721c6e1021c83641f1d75
xuewei <x@zxxw.com>
2017-02-10 15:43:10 +0800

    remove  db file  & add  htmlfile

---
+++ app/szw_nongzhuangcaizhai.app.php
@@ -0,0 +1,12 @@
+<?php
+
+/* 农庄采摘频道 */
+
+class Szw_nongzhuangcaizhaiApp extends MallbaseApp
+{
+function index()
+{
+$this->display('szw_nongzhuangcaizhai.index.html');
+}
+}
+?>
---
+++ languages/sc-utf-8/szw_lvyoujingdian.lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/* 旅游景点频道 */
+
+return array(
+    'manuname' => '旅游景点',
+	
+);
+?>
